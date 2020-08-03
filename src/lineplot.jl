9abe0ce9c9402184d6343b6953320a72fc660a9d
mcmcgrath13 <m.c.mcgrath13@gmail.com>
2020-08-03 10:50:22 -0400

    feat: wip lineplot functions

---
+++ src/lineplot.jl
@@ -0,0 +1,20 @@
+using DataFrames
+
+
+function lineplot(data; x_col=:x, y_col=:y) # assumes Tables.jl format
+    data |> @vlplot(:line, x=x_col, y=y_col)
+end
+
+function lineplot(data::AbstractVector)
+    lineplot(DataFrame(x=collect(1:length(data)), y=data))
+end
+
+
+function lineplot(data::AbstractArray{T, 2}) where {T}
+    rows, cols = size(data)
+    if rows == 1 #treat like a vector
+        lineplot(vec(data))
+    else
+        lineplot(DataFrame(x=data[:, 1], y=data[:,2]))
+    end
+end
