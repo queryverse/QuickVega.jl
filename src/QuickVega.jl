4189ab5ec2d48558195c8129eb301fed897d4bbe
davibarreira <davisbarreira@gmail.com>
2020-11-08 11:06:44 -0300

    Added scatterplot

--- src/QuickVega.jl
+++ src/QuickVega.jl
@@ -5,10 +5,12 @@ using VegaLite
 export histogram
 export lineplot
 export barplot
+export scatterplot
 
 include("histogram.jl")
 include("lineplot.jl")
 include("barplot.jl")
+include("scatterplot.jl")
 end # module
 
 
---
+++ src/scatterplot.jl
@@ -0,0 +1,20 @@
+using DataFrames
+
+
+function scatterplot(data; x_col=:x, y_col=:y) # assumes Tables.jl format
+    data |> @vlplot(:circle, x=x_col, y=y_col)
+end
+
+function scatterplot(data::AbstractVector)
+    scatterplot(DataFrame(x=collect(1:length(data)), y=data))
+end
+
+
+function scatterplot(data::AbstractArray{T, 2}) where {T}
+    rows, cols = size(data)
+    if rows == 1 #treat like a vector
+        scatterplot(vec(data))
+    else
+        scatterplot(DataFrame(x=data[:, 1], y=data[:,2]))
+    end
+end
